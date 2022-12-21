b08e990327d80e7aa22921dddedefc6b41546597
AnnaLevv <anyaa_dms@mail.ru>
2022-12-21 13:50:28 -0500

    feature added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         CarFeature XLR =new CarFeature();
         System.out.println("launch the car");
+        XLR.color="green";
+        XLR.engine=2332;
+        XLR.model="a5";
     }
 }
